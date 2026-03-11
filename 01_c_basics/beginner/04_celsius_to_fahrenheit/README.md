fb42fe2c32dc4fc6a324253adad391804e675d07
jamalmatic <jamalmatic@gmail.com>
2026-03-11 23:31:51 +0000

    Add Challenge: Celsius to Fahrenheit

---
+++ 01_c_basics/beginner/04_celsius_to_fahrenheit/README.md
@@ -0,0 +1,28 @@
+# Challenge: Celsius to Fahrenheit
+
+## Description
+
+Write a program that converts temperature from Celsius to Fahrenheit.
+
+Formula:
+
+F = (C × 9 / 5) + 32
+
+## Example
+
+Input
+
+```
+25
+```
+
+Output
+
+```
+77.00
+```
+
+## Concepts Practiced
+
+* floating point variables
+* arithmetic expressions
---
+++ 01_c_basics/beginner/04_celsius_to_fahrenheit/starter.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    float celsius;
+
+    // Ask the user to enter temperature in Celsius
+    printf("Enter temperature in Celsius: ");
+
+    // Read the Celsius value
+    scanf("%f", &celsius);
+
+    // TODO: Convert Celsius to Fahrenheit
+    // Formula: F = (C × 9 / 5) + 32
+
+    // TODO: Print the result with 2 decimal places
+
+    
+
+    return 0;
+}
