01ad40bd9a0688481f6bb58f33f6f2a289efa18d
jamalmatic <jamalmatic@gmail.com>
2026-03-11 23:13:16 +0000

    Add Challenge: Print Your Name

---
+++ 01_c_basics/beginner/02_print_name/README.md
@@ -0,0 +1,16 @@
+# Challenge: Print Your Name
+
+## Description
+
+Write a C program that prints your name.
+
+## Example Output
+
+```
+My name is Jamal
+```
+
+## Concepts Practiced
+
+* `printf`
+* string output
---
+++ 01_c_basics/beginner/02_print_name/starter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[100];
+
+    // Ask the user to enter their name
+    printf("Enter your name: ");
+
+    // Read the user's name
+    scanf("%s", name);
+
+    // TODO: Print a greeting message using the name
+    // Example: Hello, Jamal!
+
+
+    return 0;
+}
