0977794f227396e81ffa01060f8109db1e8b9cc0
jamalmatic <jamalmatic@gmail.com>
2026-03-12 00:02:44 +0000

    Add Challenge: Area of a Circle

---
+++ 01_c_basics/beginner/07_area_of_circle/README.md
@@ -0,0 +1,34 @@
+# Challenge: Area of a Circle
+
+## Description
+
+Write a program that calculates the area of a circle.
+
+Formula:
+
+Area = π × r × r
+
+Use:
+
+```
+π = 3.14159
+```
+
+## Example
+
+Input
+
+```
+5
+```
+
+Output
+
+```
+78.54
+```
+
+## Concepts Practiced
+
+* constants
+* floating point calculations
---
+++ 01_c_basics/beginner/07_area_of_circle/starter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius;
+    
+    // TODO: Declare a constant PI with value 3.14159
+
+    // Ask the user to enter the radius
+    printf("Enter the radius of the circle: ");
+
+    // Read the radius
+    scanf("%f", &radius);
+
+    //  TODO: Calculate the area of the circle using the constant
+    //  Formula: Area = PI * radius * radius
+
+    // TODO: Print the area with 2 decimal places
+
+
+    return 0;
+}
